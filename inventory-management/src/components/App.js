fb086041a2c4930d44fd7a51e8f3224f084dd89b
thamjieying <thamjieying@gmail.com>
2017-12-04 23:07:58 +0800

    solved CORS??

--- inventory-management/src/components/App.js
+++ inventory-management/src/components/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-
 import { PostList } from './Posts';
 import Test from './Test';
 import {SuppliersList} from './suppliers';
+import { Orders } from './orders'
+
+import { simpleRestClient, fetchUtils, Admin, Resource } from 'admin-on-rest';
+const httpClient = (url, options = {}) => {
+    if (!options.headers) {
+        options.headers = new Headers({ Accept: 'application/json' });
+    }
+    // add your own headers here
+    options.headers.set('Content-Range', 'posts 0-24/319');
+    return fetchUtils.fetchJson(url, options);
+}
+const restClient = simpleRestClient('http://localhost:5100', httpClient);
+
+
 
 const App = () => (
-    <Admin restClient={jsonServerRestClient('http://localhost:5100')}>
+    <Admin restClient={restClient}>
         <Resource name="Test" list={ Test } />
         <Resource name="Posts" list={ PostList } />
         <Resource name="suppliers" list={SuppliersList} />
+        <Resource name="orders" list={Orders}/>
     </Admin>
 );
 
--- inventory-management/src/components/Orders.js
+++ inventory-management/src/components/Orders.js
@@ -1,65 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { List, Datagrid, TextField } from 'admin-on-rest';
 import Moment from 'react-moment';
 
-class Orders extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      orders: []
-    }
-  }
-  componentDidMount() {
-     console.log('mounted')
-     fetch("/orders")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.orders)
-        this.setState({orders: data.orders })
-      })
-  }
-
-  render() {
-    return (
-      <List {...this.state.orders}>
-          <Datagrid>
-              <TextField source="id" />
-              <TextField source="title" />
-              <TextField source="body" />
-          </Datagrid>
-      </List>
-
-      // <div>
-      //   <h1>All Orders</h1>
-      //     <table>
-      //     <thead>
-      //       <tr>
-      //         <th>Date</th>
-      //         <th>Invoice No</th>
-      //         <th>Mode of Transport</th>
-      //         <th>Delivery Time</th>
-      //         <th>Address</th>
-      //         <th>&nbsp;</th>
-      //       </tr>
-      //       </thead>
-      //       {this.state.orders.map((order,index)=>{
-      //         return (
-      //           <tbody key={index}>
-      //             <tr>
-      //               <td><Moment format="YYYY/MM/DD">{order.date}</Moment></td>
-      //               <td>{order.invoiceNo}</td>
-      //               <td>{order.modeOfTransport}</td>
-      //               <td>{order.deliveryTime}</td>
-      //               <td>{order.deliveryAddress}</td>
-      //               <td><a href="orders/{{order.id}}">View</a><a href="orders/{{order.id}}">Delete</a></td>
-      //             </tr>
-      //           </tbody>
-      //         )})}
-      //       </table>
-      //   </div>
-    )
-  }
-}
-
-
-export default Orders
+export const Orders = (props) => (
+    <List {...props}>
+        <Datagrid>
+            <TextField source="date" />
+            <TextField source="" />
+            <TextField source="" />
+        </Datagrid>
+    </List>
+);
