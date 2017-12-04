ef07c724d3adbffdb8eed0b21e1defe5401c5185
thamjieying <thamjieying@gmail.com>
2017-12-04 21:20:14 +0800

    order changes

--- inventory-management/src/components/Orders.js
+++ inventory-management/src/components/Orders.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { List, Datagrid, TextField } from 'admin-on-rest';
+import Moment from 'react-moment';
 
 class Orders extends Component {
   constructor(props) {
@@ -19,11 +21,42 @@ class Orders extends Component {
 
   render() {
     return (
-      <div>
-        {this.state.orders.map((order)=>{
-          return <div>{order.invoiceNo}</div>
-        })}
-      </div>
+      <List {...this.state.orders}>
+          <Datagrid>
+              <TextField source="id" />
+              <TextField source="title" />
+              <TextField source="body" />
+          </Datagrid>
+      </List>
+
+      // <div>
+      //   <h1>All Orders</h1>
+      //     <table>
+      //     <thead>
+      //       <tr>
+      //         <th>Date</th>
+      //         <th>Invoice No</th>
+      //         <th>Mode of Transport</th>
+      //         <th>Delivery Time</th>
+      //         <th>Address</th>
+      //         <th>&nbsp;</th>
+      //       </tr>
+      //       </thead>
+      //       {this.state.orders.map((order,index)=>{
+      //         return (
+      //           <tbody key={index}>
+      //             <tr>
+      //               <td><Moment format="YYYY/MM/DD">{order.date}</Moment></td>
+      //               <td>{order.invoiceNo}</td>
+      //               <td>{order.modeOfTransport}</td>
+      //               <td>{order.deliveryTime}</td>
+      //               <td>{order.deliveryAddress}</td>
+      //               <td><a href="orders/{{order.id}}">View</a><a href="orders/{{order.id}}">Delete</a></td>
+      //             </tr>
+      //           </tbody>
+      //         )})}
+      //       </table>
+      //   </div>
     )
   }
 }
