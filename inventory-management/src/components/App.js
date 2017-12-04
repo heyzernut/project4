589e559dcc2870b29bd805f961dbd117a7ad2b90
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-12-04 20:20:12 +0800

    created suppliers

--- inventory-management/src/components/App.js
+++ inventory-management/src/components/App.js
@@ -3,11 +3,13 @@ import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
 import { PostList } from './Posts';
 import Test from './Test';
+import Supplier from './Supplier';
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('')}>
         <Resource name="Test" list={ Test } />
         <Resource name="Posts" list={ PostList } />
+        <Resource name="Supplier" list={ Supplier } />
     </Admin>
 );
 
