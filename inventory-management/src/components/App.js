1b7790ab8a8cc45c8feeb724de5a5910b0eebd1c
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-12-04 18:03:42 +0800

    create side nav

--- inventory-management/src/components/App.js
+++ inventory-management/src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
-import PostList from './Posts';
+import { PostList } from './Posts';
 import Test from './Test';
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('')}>
-        <Resource name="Test" list={Test} />
+        <Resource name="Test" list={ Test } />
+        <Resource name="Posts" list={ PostList } />
     </Admin>
 );
 
