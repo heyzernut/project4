a5202904263796fd85f94b23eb9b8a1c8d643111
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-12-05 13:32:08 +0800

    http config

--- inventory-management/src/components/App.js
+++ inventory-management/src/components/App.js
@@ -2,27 +2,33 @@ import React from 'react';
 import { PostList } from './Posts';
 import Test from './Test';
 import {SuppliersList} from './suppliers';
-import { Orders } from './orders'
+import authClient from './authClient';
 
-import { simpleRestClient, fetchUtils, Admin, Resource } from 'admin-on-rest';
+// import { Orders } from './orders'
+
+import { jsonServerRestClient, fetchUtils, Admin, Resource } from 'admin-on-rest';
 const httpClient = (url, options = {}) => {
     if (!options.headers) {
         options.headers = new Headers({ Accept: 'application/json' });
     }
     // add your own headers here
-    options.headers.set('Content-Range', 'posts 0-24/319');
+    options.headers.set('X-Total-Count', '319');
+    return fetchUtils.fetchJson(url, options);
+    options.user = {
+        authenticated: true,
+        token: 'SRTRDFVESGNJYTUKTYTHRG'
+    }
     return fetchUtils.fetchJson(url, options);
 }
-const restClient = simpleRestClient('http://localhost:5100', httpClient);
 
-
+const restClient = jsonServerRestClient('http://localhost:5100', httpClient);
 
 const App = () => (
-    <Admin restClient={restClient}>
+    <Admin authClient={authClient} restClient={restClient}>
         <Resource name="Test" list={ Test } />
         <Resource name="Posts" list={ PostList } />
         <Resource name="suppliers" list={SuppliersList} />
-        <Resource name="orders" list={Orders}/>
+        {/* <Resource name="orders" list={Orders}/> */}
     </Admin>
 );
 
